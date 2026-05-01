1c1af71dbeb02767c4edd5be1f09546def4d1b62
Andynium <ender3info@gmail.com>
2026-05-01 22:39:20 +0200

    Create Cocoa project in Xcode

---
+++ src/LeoTube/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LeoTube
+//
+//  Created by Administrator on 01.05.26.
+//  Copyright __MyCompanyName__ 2026. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
